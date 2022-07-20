9fe171771925b866b17f86a392251d731357f155
gabi-gabu1 <97314479+gabi-gabu1@users.noreply.github.com>
2022-07-20 13:07:11 +0200

    Added text animation for header in App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,18 @@ function App() {
     <div className="App container">
       <header className="App-header">
         <img src={booklogo} alt="logo" />
-        <h3>Dictionary</h3>
+        <div className="waviy">
+          <span object="--i:1">D</span>
+          <span object="--i:2">i</span>
+          <span object="--i:3">c</span>
+          <span object="--i:4">t</span>
+          <span object="--i:5">i</span>
+          <span object="--i:6">o</span>
+          <span object="--i:7">n</span>
+          <span object="--i:8">a</span>
+          <span object="--i:8">r</span>
+          <span object="--i:8">y</span>
+        </div>
       </header>
       <Dictionary keyWord="moon" />
     </div>
